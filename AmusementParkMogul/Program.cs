f6a778cd42cd44858d07d80e87a29556110fe63d
Morgan Plagenz <plagenzm@yahoo.com>
2024-09-02 16:36:22 -0500

    first commit

---
+++ AmusementParkMogul/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmusementParkMogul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //creating a rollercoaster object
+            Rollercoaster rollercoaster = new Rollercoaster();
+            rollercoaster.Name = "Viper";
+            rollercoaster.Color = "black";
+            rollercoaster.Material = "wood";
+            rollercoaster.Length = 200;
+            rollercoaster.CalculateRollercoasterCost();
+            rollercoaster.getRollercoasterInfo();
+
+
+
+
+            //had to put this in or the console would automatically close
+            Console.ReadLine();
+        }
+    }
+}
